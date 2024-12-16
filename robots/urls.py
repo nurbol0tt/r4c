927a75be40bc098d2227c1237d3dd445fd50904b
nurbol0tt <fairtail764@gmail.com>
2024-12-16 22:33:44 +0600

    add api for download report

--- robots/urls.py
+++ robots/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import create_robot
+from .views import create_robot, download_robot_summary, download_summary_page
 
 urlpatterns = [
     path('create-robot/', create_robot, name='create_robot'),
+    path('download-robot-summary/', download_robot_summary, name='download_robot_summary'),
+    path('download-summary-page/', download_summary_page, name='download_summary_page'),
 ]
--- robots/views.py
+++ robots/views.py
@@ -1,10 +1,15 @@
 import json
-from django.shortcuts import render, redirect
+import datetime
+
+from django.db.models.aggregates import Count
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from openpyxl import Workbook
+from .models import Robot
 
 from .forms import RobotForm
-from .models import Robot
 
 @csrf_exempt
 def create_robot(request):
@@ -22,3 +27,46 @@ def create_robot(request):
     else:
         form = RobotForm()
     return render(request, 'robots.html', {'form': form})
+
+def download_robot_summary(request):
+    today = datetime.date.today()
+    last_week_start = today - datetime.timedelta(days=7)
+
+    # Группировка данных и подсчёт записей
+    data = Robot.objects.all().values(
+        'model', 'version'
+    ).annotate(total=Count('id')).order_by('model', 'version')
+
+    wb = Workbook()
+    sheet_mapping = {}
+
+    for entry in data:
+        model = entry['model']
+        version = entry['version']
+        total = entry['total']
+
+        if model not in sheet_mapping:
+            sheet = wb.create_sheet(title=model)
+            sheet_mapping[model] = sheet
+
+            sheet.append(['Модель', 'Версия', 'Количество за неделю'])
+
+        sheet_mapping[model].append([model, version, total])
+
+    if not sheet_mapping:
+        default_sheet = wb.active
+        default_sheet.title = "Summary"
+        default_sheet.append(["Нет данных за последние 7 дней"])
+
+    if "Sheet" in wb.sheetnames and len(wb.sheetnames) > 1:
+        wb.remove(wb["Sheet"])
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = f'attachment; filename="robot_summary_{today}.xlsx"'
+    wb.save(response)
+
+    return response
+
+
+def download_summary_page(request):
+    return render(request, 'upload.html')
